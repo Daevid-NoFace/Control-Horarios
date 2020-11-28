064aab48e3e75cec2cf195490de557eb0b92d621
Rafael <gayoso0597@gmail.com>
2020-11-29 00:57:06 +0100

    Added SlideShow

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,5 +1,8 @@
 package controller;
 
+import com.jfoenix.controls.JFXProgressBar;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
@@ -8,8 +11,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.util.Duration;
 import model.Controller;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
@@ -17,17 +24,39 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    int count =0;
+    @FXML
+    private ImageView slideShow;
+
+    private ArrayList<Image> images;
 
     @FXML
     private Label resultLabel;
 
     @FXML
-    private ProgressBar progresBar;
+    private JFXProgressBar progressBar;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         resultLabel.setText(" ");
-        progresBar.setProgress(0);
+        progressBar.setProgress(0);
+    }
+
+    private void createSlideShow(){
+
+        images = new ArrayList<>();
+        images.add(new Image("src/resources/palobiofarma.png"));
+        images.add(new Image("src/resources/medibiofarma.png"));
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(3),event -> {
+            slideShow.setImage(images.get(count));
+            count++;
+            if(count == 2){
+                count = 0;
+            }
+        }));
+
     }
 
     public void mergeExcel(ActionEvent actionEvent) {
@@ -51,7 +80,7 @@ public class MainController implements Initializable {
             longTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
                 @Override
                 public void handle(WorkerStateEvent event) {
-                    progresBar.setProgress(100);
+                    progressBar.setProgress(100);
                     resultLabel.setText("DONE!!!");
                 }
             });
@@ -60,7 +89,7 @@ public class MainController implements Initializable {
                 @Override
                 public void handle(WorkerStateEvent event) {
                     resultLabel.setText("Working on it");
-                    progresBar.setProgress(longTask.getProgress()); //el progressbar este esta al berro
+                    progressBar.setProgress(longTask.getProgress()); //el progressbar este esta al berro
                 }
             });
 
