1807768eb8a8ec59d987c823dab5e9aa5407a3a5
David <dmartinezgonzalez524@gmail.com>
2020-12-01 01:45:53 +0100

    construccion de horarios por cada empleado

--- src/services/EmpleadoServices.java
+++ src/services/EmpleadoServices.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class EmpleadoServices {
 
-    public static ArrayList<Empleado> listadoEmpleados(){
+    public ArrayList<Empleado> listadoEmpleados(){
         ArrayList<Empleado> lista= new ArrayList<>();
         try{
             Connection conexion = ServicesLocator.getConnection();
@@ -31,5 +31,29 @@ public class EmpleadoServices {
             e.printStackTrace();
         }
         return lista;
+    }
+
+    public ArrayList<Empleado> listadoEmpleadosXEmpresa(String nombreEmpresa) {
+        ArrayList<Empleado> list = new ArrayList<>();
+
+        try {
+            Connection connection = ServicesLocator.getConnection();
+            String consulta = "Select empleado.* from empleado join empresa on empleado.cod_empresa = empresa.cod_empresa where empresa.nombre_empresa = " + "'" + nombreEmpresa + "'";
+            PreparedStatement prepare = connection.prepareStatement(consulta);//para consultas
+            prepare.execute();
+            ResultSet result = prepare.getResultSet();//para quedarme con lo q devuelve la consulta
+            while (result.next()){ //para varias filas
+                Empleado a = new Empleado(result.getInt(1), result.getString(2),
+                        result.getString(3),result.getString(4),result.getString(5),
+                        result.getString(6),result.getInt(7));
+                list.add(a);
+            }
+
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
--- src/services/EmpresaServices.java
+++ src/services/EmpresaServices.java
@@ -1,12 +1,39 @@
 package services;
 
+import model.Empleado;
+import model.Empresa;
+
 import java.io.*;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class EmpresaServices {
+
+    public ArrayList<Empresa> listadoEmpresas(){
+        ArrayList<Empresa> lista= new ArrayList<>();
+        try{
+            Connection conexion = ServicesLocator.getConnection();
+            String consulta = "Select empresa.* from empresa";
+            PreparedStatement prepare = conexion.prepareStatement(consulta);//para consultas
+            prepare.execute();
+            ResultSet result = prepare.getResultSet();//para quedarme con lo q devuelve la consulta
+            while (result.next()){ //para varias filas
+                Empresa a = new Empresa(result.getInt(1), result.getString(2),
+                        result.getString(3),result.getString(4),result.getString(5));
+                lista.add(a);
+            }
+            conexion.close();
+        }
+        catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
 
     public void readPicture(int materialId, String filename) {
         // update sql
